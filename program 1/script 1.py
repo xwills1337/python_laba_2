d99fe7aeed5e89dc0bf2b698ea329ca2fe90c8ec
xwills1337 <misha.sid@rambler.ru>
2022-10-22 21:24:39 +0400

    create Y.csv

--- program 1/script 1.py
+++ program 1/script 1.py
@@ -1,8 +1,6 @@
 import csv
 import pandas as pd
 df = pd.read_csv('../data.csv', sep=';', encoding='cp1251')
-print(df)
-
-df.to_csv('data2.csv', sep=';', encoding='cp1251', index=False)
-
 df['Дата'].to_csv('X.csv', sep=';', encoding='cp1251', index=False)
+del df['Дата']
+df.to_csv('Y.csv', sep=';', encoding='cp1251', index=False)
