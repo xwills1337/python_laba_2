3bbfad346927456bf404278576982a7e42217dee
xwills1337 <misha.sid@rambler.ru>
2022-10-22 21:21:13 +0400

    create X.csv

--- program 1/script 1.py
+++ program 1/script 1.py
@@ -1,4 +1,8 @@
 import csv
 import pandas as pd
-df = pd.read_csv('data.csv', encoding='cp1251')
+df = pd.read_csv('../data.csv', sep=';', encoding='cp1251')
 print(df)
+
+df.to_csv('data2.csv', sep=';', encoding='cp1251', index=False)
+
+df['Дата'].to_csv('X.csv', sep=';', encoding='cp1251', index=False)
