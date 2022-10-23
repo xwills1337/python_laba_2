4028b6012f770a0b565b37fefe3bc3debb17e623
xwills1337 <misha.sid@rambler.ru>
2022-10-23 14:52:55 +0400

    division by week

--- program 3/script 3.py
+++ program 3/script 3.py
@@ -1,2 +1,14 @@
 import pandas as pd
 df = pd.read_csv('../data.csv', sep=';', encoding='cp1251')
+
+mas=list(range(7))
+max=df.shape[0]
+while (max>7):
+
+    lf = df.loc[mas[0]:mas[-1]]
+    print(lf)
+
+    mas = [i + 7 for i in mas]
+    max=max-7
+if (max>0):
+    print(df.loc[df.shape[0]-max:df.shape[0]-1])
