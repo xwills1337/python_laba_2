67bb31478f2f3dffa8a90f35a0f7ee3797b74b37
xwills1337 <misha.sid@rambler.ru>
2022-10-23 23:26:35 +0400

    add function 'get_data_x_y'

--- program 4/script 4.py
+++ program 4/script 4.py
@@ -1,2 +1,20 @@
 import pandas as pd
-df = pd.read_csv('../data.csv', sep=';', encoding='cp1251')
+import datetime
+def get_data_x_y(name_x: str, name_y: str, date: datetime.date) -> list[str] or None:
+    df_x = pd.read_csv(name_x, sep=';', encoding='cp1251')
+    df_y = pd.read_csv(name_y, sep=';', encoding='cp1251')
+    index = -1
+    for i in range(df_x.shape[0]):
+        if (df_x['Дата'].iloc[i].replace('.', '')==str(date).replace('-', '')):
+            index=i
+            break
+    if (index>=0):
+        return df_y.iloc[index]
+    return None
+
+name1 = '../program 1/X.csv'
+name2 = '../program 1/Y.csv'
+valid_date=datetime.date(2008, 1, 10)
+invalid_date=datetime.date(2008, 1, 9)
+print(get_data_x_y(name1,name2,valid_date))
+print(get_data_x_y(name1,name2,invalid_date))
