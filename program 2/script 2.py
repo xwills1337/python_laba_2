29517218008de1b833c095fca84df4f8f8e43658
xwills1337 <misha.sid@rambler.ru>
2022-10-23 12:57:09 +0400

    delete extra str

--- program 2/script 2.py
+++ program 2/script 2.py
@@ -1,15 +1,14 @@
 import csv
 import pandas as pd
-
+from datetime import datetime, date, time
 df = pd.read_csv('../data.csv', sep=';', encoding='cp1251')
-first_year=int(df['Дата'][0][0]+df['Дата'][0][1]+df['Дата'][0][2]+df['Дата'][0][3])
-last_year=int(df['Дата'][df.shape[0]-1][0]+df['Дата'][df.shape[0]-1][1]+df['Дата'][df.shape[0]-1][2]\
-                                                                       +df['Дата'][df.shape[0]-1][3])
+
 df.insert(0, "data_2", 0)
 for i in range(df.shape[0]):
     str_1=df['Дата'][i]
     df.loc[i,'data_2']=int(str_1[0]+str_1[1]+str_1[2]+str_1[3])
-
+first_year=df['data_2'][0]
+last_year=df['data_2'][df.shape[0]-1]
 for first_year in range(first_year, last_year+1):
      lf=df[df['data_2']==first_year]
      data=lf['Дата'].iloc[0].replace('.', '')
