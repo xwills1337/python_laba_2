a7bb6dd330e7e139deb1542897c5c0bad485eafd
xwills1337 <misha.sid@rambler.ru>
2022-10-24 00:44:38 +0400

    add function 'get_data'

--- program 4/script 4.py
+++ program 4/script 4.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import datetime
 import os
+
+def get_data(name: str, date: datetime.date) -> list[str] or None:
+    df = pd.read_csv(name, sep=';', encoding='cp1251')
+    for i in range(df.shape[0]):
+        if (df['Дата'].iloc[i].replace('.', '')==str(date).replace('-', '')):
+            return df.iloc[i]
 def get_data_x_y(name_x: str, name_y: str, date: datetime.date) -> list[str] or None:
     df_x = pd.read_csv(name_x, sep=';', encoding='cp1251')
     df_y = pd.read_csv(name_y, sep=';', encoding='cp1251')
@@ -22,13 +28,15 @@ def get_data_year_or_month(name_y_or_m: str, date: datetime.date) -> list[str] o
                     return df.iloc[i]
     return None
 
-
+name0 = '../data.csv'
 name1 = '../program 1/X.csv'
 name2 = '../program 1/Y.csv'
 name3 = '../program 2/'
 name4 = '../program 3/'
 valid_date=datetime.date(2008, 1, 10)
 invalid_date=datetime.date(2008, 1, 9)
+#print(get_data(name0,valid_date))
+#print(get_data(name0,invalid_date))
 #print(get_data_x_y(name1,name2,valid_date))
 #print(get_data_x_y(name1,name2,invalid_date))
 #print(get_data_year_or_month(name3, valid_date))
