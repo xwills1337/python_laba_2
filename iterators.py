408db85f02cf8ff744f1a33fdb0542296d03df15
xwills1337 <misha.sid@rambler.ru>
2022-10-24 04:42:31 +0400

    create class 'DateIterator_X_Y'

--- iterators.py
+++ iterators.py
@@ -3,7 +3,6 @@ import datetime
 import os
 
 class DateIterator:
-
     def __init__(self):
 
         self.counter = 0
@@ -19,8 +18,25 @@ class DateIterator:
                                                       self.df.loc[self.counter - 1][3], self.df.loc[self.counter - 1][4],
                                                       self.df.loc[self.counter - 1][5], self.df.loc[self.counter - 1][6])
 
+class DateIterator_X_Y:
+    def __init__(self):
+
+        self.counter = 0
+        self.lf = pd.read_csv('program 1/X.csv', sep=';', encoding='cp1251')
+        self.df = pd.read_csv('program 1/Y.csv', sep=';', encoding='cp1251')
+    def __next__(self) -> tuple:
+        if self.counter == self.df.shape[0]:
+            return (self.lf.loc[self.counter - 1][0], self.df.loc[self.counter - 1][0], self.df.loc[self.counter - 1][1],
+                                                      self.df.loc[self.counter - 1][2], self.df.loc[self.counter - 1][3],
+                                                      self.df.loc[self.counter - 1][4], self.df.loc[self.counter - 1][5])
+        elif self.counter < self.df.shape[0]:
+            self.counter += 1
+            return (self.lf.loc[self.counter - 1][0], self.df.loc[self.counter - 1][0], self.df.loc[self.counter - 1][1],
+                                                      self.df.loc[self.counter - 1][2], self.df.loc[self.counter - 1][3],
+                                                      self.df.loc[self.counter - 1][4], self.df.loc[self.counter - 1][5])
+
 if __name__ == "__main__":
-    obj = DateIterator()
+    obj = DateIterator_X_Y()
     while(True):
         print(next(obj))
 
